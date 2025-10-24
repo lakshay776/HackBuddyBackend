f1af9de39741bd4bae9b7e0cba670728c5b354ae
lakshay776 <jagga.lakshay776@gmail.com>
2025-10-24 10:40:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app=express()
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:3000'],
+    origin: ['https://hack-buddy-5ui9fh1dh-lakshay-jaggas-projects.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
